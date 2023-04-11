4235b92a05e4d6e1ceae35cb5cfc5773c7fd2b8d
Paul Wilson <paul.wilson.r@gmail.com>
2023-04-11 14:28:07 -0500

    take out sample text

--- cv-project/src/App.js
+++ cv-project/src/App.js
@@ -9,40 +9,11 @@ class App extends Component {
     super(props);
     this.state = {
       infoProp: {
-        name: 'Name Name',
-        phone: '5555555555',
-        email: 'email@email.com'
       },
       experienceProp: [
-        {
-          jobKey: 'job1',
-          jobTitle: 'Test job',
-          jobDescription: 'This is the description of the job that was done. It was good job I did',
-          hired: '2006-1',
-          quit: '2007-5'
-        },
-        {
-          jobKey: 'job2',
-          jobTitle: 'Test job 2',
-          jobDescription: 'This is the second description of the job that was done. It was bad job I did',
-          hired: '2009-7',
-          quit: '2010-8'
-        },
         
       ],
       educationProp: [
-        {
-          schoolKey: 'school1',
-          schoolName: 'Test School',
-        major: 'All majors',
-        graduation: '2009'
-        },
-        {
-          schoolKey: 'school2',
-          schoolName: 'Test School 2',
-        major: 'None majors',
-        graduation: '2019'
-        },
         
       ]
       
