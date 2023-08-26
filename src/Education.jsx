bcf2c20965adb3b3976caab10097b63c6052d85f
Paul Wilson <paul.wilson.r@gmail.com>
2023-08-25 21:45:52 -0500

    reworks education form

--- src/Education.jsx
+++ src/Education.jsx
@@ -1,18 +1,59 @@
 import { useState } from 'react'
+import uniqid from 'uniqid'
 
-/* eslint-disable react/prop-types */
 export default function EducationDetails ({ handleChange, eduInfo }) {
- const [changingEduInfo, setChangingEduInfo] = useState([...eduInfo])
-  const [currentView, setCurrentView] = useState(0)
-  const [currentEduKey, setCurrentEduKey] = useState(1)
-  function handleClick(key) {
-    let schoolIndex = changingEduInfo.findIndex((school => school.key === key)) +1;
-    setCurrentView(schoolIndex);
+  const [editOpen, setEditOpen] = useState(false)
+  const [currentSchool, setCurrentSchool] = useState({})
+
+  function findSchoolIndex (schoolKey) {
+    let schoolIndex = eduInfo.findIndex(school => school.key === schoolKey)
+    return schoolIndex
   }
 
-  function handleDelete() {
+  function handleClick (schoolKey) {
+    let schoolIndex = findSchoolIndex(schoolKey)
+    let shownSchool = eduInfo[schoolIndex]
+    setCurrentSchool(shownSchool)
+    setEditOpen(true)
   }
-  if (currentView === 0) {
+
+  function handleDelete () {
+    let schoolIndex = findSchoolIndex(currentSchool.key);
+    let tempEduInfo = [...eduInfo];
+    tempEduInfo.splice(schoolIndex, 1);
+    handleChange({education: tempEduInfo});
+    setEditOpen(false)
+  }
+
+  function handleCancel () {
+    setEditOpen(false)
+  }
+
+  function handleSave () {
+    let schoolIndex = findSchoolIndex(currentSchool.key);
+    let tempEduInfo = [...eduInfo];
+    tempEduInfo[schoolIndex] = currentSchool;
+    handleChange({education: tempEduInfo})
+    setEditOpen(false)
+  }
+
+  function handleAddNew () {
+    let tempEduInfo = [...eduInfo];
+    let newSchool = {
+      schoolName: 'New School',
+      study: '',
+      schoolStart: '',
+      schoolEnd: '',
+      key: uniqid(),
+    }
+    tempEduInfo.push(newSchool);
+    handleChange({education: tempEduInfo});
+    return;
+    
+    
+  }
+
+  if (!editOpen) {
     return (
       <div className='formContainer'>
         <div className='formTitle'>
@@ -20,84 +61,82 @@ export default function EducationDetails ({ handleChange, eduInfo }) {
           <img src='public/arrow.png' alt='minimize' className='arrow' />
         </div>
         {eduInfo.map(school => {
-          return <p onClick={() => {
-            handleClick(school.key)
-          }} key = {school.key}>{school.schoolName}</p>
+          return (
+            <p
+              onClick={() => {
+                handleClick(school.key)
+              }}
+              key={school.key}
+            >
+              {school.schoolName}
+            </p>
+          )
         })}
-        <button>Add Education</button>
+        <button onClick={() => {
+          handleAddNew();
+          }}>Add Education</button>
       </div>
-      
     )
   } else {
-
-    const currentSchool= {...changingEduInfo[currentView-1]}
-    console.log(currentSchool)
-  return (
-    <div className='formContainer'>
-      <div className='formTitle'>
-        <h2>Education</h2>
-        <img src='public/arrow.png' alt='minimize' className='arrow' />
+    return (
+      <div className='formContainer'>
+        <div className='formTitle'>
+          <h2>Education</h2>
+          <img src='public/arrow.png' alt='minimize' className='arrow' />
+        </div>
+        <label>
+          School Name
+          <input
+            type='text'
+            id='schoolName'
+            name='schoolName'
+            value={currentSchool.schoolName}
+            onChange={e => {
+              setCurrentSchool({ ...currentSchool, schoolName: e.target.value })
+            }}
+          ></input>
+        </label>
+        <label>
+          Area of Study
+          <input
+            type='text'
+            id='study'
+            name='study'
+            value={currentSchool.study}
+            onChange={e => {
+              setCurrentSchool({ ...currentSchool, study: e.target.value })
+            }}
+          ></input>
+        </label>
+        <label>
+          From
+          <input
+            type='month'
+            id='schoolStart'
+            name='schoolStart'
+            value={currentSchool.schoolStart}
+            onChange={e => {
+              setCurrentSchool({ ...currentSchool, schoolStart: e.target.value })
+            }}
+          ></input>
+        </label>
+        <label>
+          To
+          <input
+            type='month'
+            id='schoolEnd'
+            name='schoolEnd'
+            value={currentSchool.schoolEnd}
+            onChange={e => {
+              setCurrentSchool({ ...currentSchool, schoolEnd: e.target.value })
+            }}
+          ></input>
+        </label>
+        <button onClick={handleDelete}>Delete</button>
+      <button onClick={handleCancel}>Cancel</button>
+      <button onClick={handleSave}>Save</button>
       </div>
-      <label>
-        School Name
-        <input
-          type='text'
-          id='schoolName'
-          name='schoolName'
-          value={currentSchool.schoolName}
-          onChange={e => {
-            currentSchool.schoolName = e.target.value;
-            setChangingEduInfo(changingEduInfo.toSpliced(currentView-1, 1, currentSchool));
-          }}
-        ></input>
-      </label>
-      <label>
-        Area of Study
-        <input
-          type='text'
-          id='study'
-          name='study'
-          value={currentSchool.study}
-          onChange={e => {
-            currentSchool.study = e.target.value
-            setChangingEduInfo(changingEduInfo.toSpliced(currentView-1, 1, currentSchool));          }}
-        ></input>
-      </label>
-      <label>
-        From
-        <input
-          type='month'
-          id='schoolStart'
-          name='schoolStart'
-          value={currentSchool.schoolStart}
-          onChange={e => {
-            currentSchool.schoolStart = e.target.value
-            setChangingEduInfo(changingEduInfo.toSpliced(currentView-1, 1, currentSchool));          }}
-        ></input>
-      </label>
-      <label>
-        To
-        <input
-          type='month'
-          id='schoolEnd'
-          name='schoolEnd'
-          value={currentSchool.schoolEnd}
-          onChange={e => {
-            currentSchool.schoolEnd = e.target.value
-            setChangingEduInfo(changingEduInfo.toSpliced(currentView-1, 1, currentSchool)); 
-          }}
-        ></input>
-      </label>
-      <button onClick={() => {
-        handleDelete();
-        setCurrentView(0);
-      }}>Delete</button>
-      <button onClick={() => setCurrentView(0)}>Cancel</button>
-      <button onClick={() => {
-        handleChange({ education: changingEduInfo })
-        setCurrentView(0);
-        }}>Save</button>
-    </div>
-  )
+    )
+  }
 }
-}
+
